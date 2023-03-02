724f58f4a9127cf09e0352868e25cf2825cb66a5
Malcolmt129 <malcolm.t129@gmail.com>
2023-03-02 18:40:23 -0500

    Made VendingMachineTest class. Created the set up of the teest case and tested the the constructor

---
+++ VendingMachine/src/edu/towson/cis/cosc442/project2/vendingmachine/VendingMachineTest.java
@@ -0,0 +1,63 @@
+package edu.towson.cis.cosc442.project2.vendingmachine;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class VendingMachineTest {
+	
+	VendingMachine vm;
+	
+	
+	@Before
+	public void setUp() throws Exception {
+		vm = new VendingMachine();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testVendingMachine() {
+		assertEquals(vm.getBalance(),0.0,0.001);
+	}
+
+	@Test
+	public void testAddItem() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetItem() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveItem() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInsertMoney() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBalance() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMakePurchase() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReturnChange() {
+		fail("Not yet implemented");
+	}
+
+}
